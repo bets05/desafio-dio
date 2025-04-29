da49c386a62d957eec015d4e73a15326033272ec
luizfabiano <luizfdo2005@gmail.com>
2025-04-29 07:02:36 -0300

    Desafio Realizado

---
+++ desafio/desafio.js
@@ -0,0 +1,30 @@
+let heroi = "Luiz";
+let xp = 1000;
+let nivel = "";
+
+if (xp <= 1000) {
+    nivel = "Ferro"
+}
+if (xp >= 1001 && xp <= 2000){
+    nivel = "Bronze"
+}
+if (xp >= 2001 && xp <= 5000){
+    nivel = "Prata" 
+}
+if (xp >= 5001 && xp <= 7000){
+    nivel = "Ouro"
+}
+if (xp >= 7001 && xp <= 8000){
+    nivel = "Platina"
+}
+if (xp >= 8001 && xp <= 9000){
+    nivel = "Ascedente"
+}
+if (xp >= 9001 && xp <= 10000){
+    nivel = "Imortal"
+}
+if (xp >= 10001){
+    nivel = "Radiante"
+}
+
+console.log(`O Herói ${heroi} está no nível ${nivel}!`);
